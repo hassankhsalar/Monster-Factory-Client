e9b95f4c2c998f22322ea0bb3f3595ffc5b17873
Hassan kh salar <khsalarhassan@gmail.com>
2025-02-18 18:42:08 +0600

    fixed home responsive

--- src/Pages/Home/AboutUs.jsx
+++ src/Pages/Home/AboutUs.jsx
@@ -2,12 +2,12 @@ import { Link } from "react-router-dom";
 
 const AboutUs = () => {
   return (
-    <div id="aboutus">
-      <h2 className="text-6xl">About Us</h2>
+    <div className="mt-4 mb-48 sm:mb-10" id="aboutus">
+      <h2 className="text-6xl text-center ">About Us</h2>
       <div className="flex mt-10 h-96 xl:h-[650px] w-9/12 mx-auto justify-center item-center gap-8">
         <div className="w-1/2 container flex items-center justify-center overflow-hidden bg-orange-600 rounded-3xl">
           <img className="object-contain relative " src="./about-banner.png" alt="" />
-          <img className="object-contain w-96 xl:w-[600px] absolute" src="./about-circle-one.png" alt="" />
+          <img className="object-contain hidden md:block w-96 xl:w-[600px] absolute" src="./about-circle-one.png" alt="" />
           
         </div>
 
@@ -23,7 +23,7 @@ const AboutUs = () => {
           </p>
           <button
             type="button"
-            className="px-8 py-3 ml-52 w-44 font-semibold rounded-full dark:bg-gray-800 dark:text-gray-100"
+            className="px-8 py-3 text-center w-44 font-semibold rounded-full dark:bg-gray-800 dark:text-gray-100"
           >
             <Link to='/aboutus'>Explore More</Link>
           </button>
